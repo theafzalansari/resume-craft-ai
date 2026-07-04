40d9d7ff86ed0b7f249179d47811d0f237f8b332
Afzal Ansari <theafzalansari@gmail.com>
2026-07-04 18:38:21 +0530

    feat: added add resume component

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {PlusSquare} from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { v4 as uuidv4 } from 'uuid';
 
 const AddResume = () => {
+    const [openDialog, setOpenDialog] = useState(false)
+    const [resumeTitle, setResumeTitle] = useState();
+
+    const onCreate=()=>{
+        const uuid= uuidv4();
+        console.log(resumeTitle, uuid);
+    }
+
     return (
         <div>
             <div className='p-14 py-24 border items-center flex
-             justify-center bg-secondary rounded-lg h-[280px]'>
+             justify-center bg-secondary rounded-lg h-[280px]
+             hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed'
+             onClick={() => setOpenDialog(true)}>
                 <PlusSquare/>
             </div>
+
+            <Dialog open={openDialog}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Create New Resume</DialogTitle>
+                        <DialogDescription>
+
+                            <p>Add a title for your new resume</p>
+                            <Input className="my-2" placeholder="Ex.Full Stack resume"
+                            onChange={(e)=>setResumeTitle(e.target.value)}
+                            />
+
+                        </DialogDescription>
+
+                        <div className='flex justify-end gap-3'>
+                            <Button onClick={()=>setOpenDialog(false)} variant="ghost">Cancel</Button>
+                            <Button
+                                disabled={!resumeTitle}
+                                onClick={()=>onCreate()}>Create</Button>
+                        </div>
+
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
         </div>
     )
 }
