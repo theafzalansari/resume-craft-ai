7a01081bb82674ba5be1979a5e476741d29b7579
Afzal Ansari <theafzalansari@gmail.com>
2026-07-10 22:50:37 +0530

    style: added cover img  and improved some styling

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -5,8 +5,13 @@ import {Link} from "react-router-dom";
 const ResumeCardItem = ({resume}) => {
     return (
         <Link to={`/dashboard/resume/`+resume.documentId+"/edit"}>
-            <div className='p-14 bg-secondary flex items-center justify-center h-[280px] border border-primary rounded-lg hover:scale-100 transition-all hover:shadow-md shadow-primary'>
-                <Notebook/>
+            <div className='p-14 border-t-[15px]
+
+          bg-gradient-to-r from-red-500 to-sky-500
+
+             flex items-center justify-center h-[280px] border border-primary rounded-lg hover:scale-100 transition-all hover:shadow-md shadow-primary'>
+                {/*<Notebook/>*/}
+                <img src="/cv.png"  width={80} height={80} />
             </div>
             <h3 className='text-center my-2'>{resume.title}</h3>
         </Link>
